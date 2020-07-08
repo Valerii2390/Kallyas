f379c2d83232f8414f79487c202a60ec8791b39f
Valerii2390-dev <valezh14@yahoo.com>
2020-07-08 14:00:05 +0300

    added header slider

---
+++ app/js/main.js
@@ -0,0 +1,32 @@
+$(function() {
+    $('.slider__inner').slick({
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        // responsive: [{
+        //         breakpoint: 1900,
+        //         settings: {
+        //             slidesToShow: 3,
+        //             slidesToScroll: 3,
+        //             infinite: true,
+        //             dots: true
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 1441,
+        //         settings: {
+        //             slidesToShow: 2,
+        //             slidesToScroll: 2
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 1000,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1
+        //         }
+        //     }
+        // ]
+    });
+});
